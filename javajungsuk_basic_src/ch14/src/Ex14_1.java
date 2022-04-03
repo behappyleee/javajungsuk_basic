4e3143aaca8790439e51f208ec30aa750602e69f
leechulhan <dhfhfkwhdk@naver.com>
2022-04-03 16:03:32 +0900

    ch_14 프로젝트 이름변경 Commit

--- javajungsuk_basic_src/ch14/src/Ex14_1.java
+++ javajungsuk_basic_src/ch14/src/Ex14_1.java
@@ -3,6 +3,17 @@ interface MyFunction {
 	void run();  // public abstract void run();
 }
 
+// Lambda Expression 
+// 함수 (메서드)를 간단한 식(expression) 으로 표현 하는 방법
+// 람다식은 익명 함수
+// 일반 함수 --> 람다식 변경 방법 = 반환 타입, 이름을 지움 
+// 함수와 메서드의 차이는 근본적으로 동일 하지만 메서드는 객체지향개념 용어, 함수는 일반적 용어
+// 클래스 안에 있으면 메서드(클래스 안에 종속적) 클래스 안에 포함되지 않은 것은 함수 (클래스에서 독립적)
+// 반환값이 있는 경우 식이나 값만 적고 return 문 생략 가능 (끝에 ; 을 붙이지 않음)
+
+// 람다식 작성 시 주의 사항 
+
+
 class Ex14_1 {
 	static void execute(MyFunction f) { // 매개변수의 타입이 MyFunction인 메서드
 		f.run();
